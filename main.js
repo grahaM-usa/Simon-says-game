a50b213c9a3a379c20371d500618a90891941b5f
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-01-24 12:28:24 -0500

    Start Js

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ const highScoreText = document.getElementById("high-score");
 const levelText = document.getElementById("level");
 
 const tiles = ["green", "red", "yellow", "blue"];
+
+startButton.addEventListener("click", startGame);
+board.addEventListener("click", (event) => {
+  const { tile } = event.target.dataset;
+  if (tile) handleClick(tile);
+});
+
+function startGame() {
+  startButton.classList.add("hidden");
+  info.innerText = "Watch the sequence carefully!";
+  level = 0;
+  document.body.style.background = "linear-gradient(to top, #87b7ff, #6f7cf5)";
+  levelUp();
+}
